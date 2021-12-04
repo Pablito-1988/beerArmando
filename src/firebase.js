3dfbd1d92f54349668ffae334415c6b2718127c4
Pablo Armando <pabloar24.7@hmail.com>
2021-12-03 21:36:05 -0300

    deleted console logs

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-console.log(process.env.REACT_APP_apiKey)
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
